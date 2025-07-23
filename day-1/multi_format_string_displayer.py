23d63cc4f8ea06819116cf55c58c1b92e8583b93
ravipandeydu <ravipandeydu@gmail.com>
2025-07-23 22:50:09 +0530

    multi_format_string_displayer Added

---
+++ day-1/multi_format_string_displayer.py
@@ -0,0 +1,7 @@
+name="Alice"
+age=30
+city="Delhi"
+
+print(f"My name is {name}, I am {age} years old, and I live in {city}.")
+print("My name is {}, I am {} years old, and I live in {}.".format(name, age, city))
+print("My name is %s, I am %d years old, and I live in %s." % (name, age, city))
