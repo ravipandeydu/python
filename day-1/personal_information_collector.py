8dec01df59341f11a45364a022a68625dfe23a80
ravipandeydu <ravipandeydu@gmail.com>
2025-07-23 10:54:45 +0530

    Q12 Added

---
+++ day-1/personal_information_collector.py
@@ -0,0 +1,8 @@
+name = input("Enter your full name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+hobby = input("Enter your hobby: ")
+
+print(f"Hello, {name}!")
+print(f"You are {age} years old and lives in {city}.")
+print(f"In your free time, you enjoy {hobby}.")
