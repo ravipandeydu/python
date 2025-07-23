b6c47d9c1b2a545891883d982bed09114890f7af
ravipandeydu <ravipandeydu@gmail.com>
2025-07-23 23:12:19 +0530

    higher_order_function_implementation Added

---
+++ day-1/higher_order_function_implementation.py
@@ -0,0 +1,11 @@
+def custom_map(func, iterable):
+    return [func(item) for item in iterable]
+
+def custom_filter(func, iterable):
+    return [item for item in iterable if func(item)]
+
+def custom_reduce(func, iterable):
+    result = iterable[0]
+    for x in iterable[1:]:
+        result = func(result, x)
+    return result
